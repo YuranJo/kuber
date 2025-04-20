d40eb009a4c81cd59e1f426e0266695b42479233
YuranJo <y14r06a062006@gmail.com>
2025-04-20 17:29:38 +0200

    🪰🪰🪰🪰🪰🪰

--- frontend/src/ThemWrapper.tsx
+++ frontend/src/ThemWrapper.tsx
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import './NavBar.css'
-import './index.css'
-import darkModeSvg from './assets/dark_mode.svg'
-import lightModeSvg from './assets/light_mode.svg'
+import './NavBar.css';
+import './index.css'; 
+
+import darkModeSvg from './assets/dark_mode.svg';
+import lightModeSvg from './assets/light_mode.svg';
+
 const ThemeWrapper: React.FC = () => {
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState(() => {
+    const savedTheme = localStorage.getItem('theme');
+    return savedTheme || 'light'; 
+  });
 
   useEffect(() => {
     if (theme === 'dark') {
@@ -13,26 +18,24 @@ const ThemeWrapper: React.FC = () => {
       document.body.classList.remove('dark-theme');
     }
 
+    localStorage.setItem('theme', theme);
+
     return () => {
       document.body.classList.remove('dark-theme');
     };
-  }, [theme]);
-
+  }, [theme]); 
   const toggleTheme = () => {
     setTheme(currentTheme => (currentTheme === 'light' ? 'dark' : 'light'));
   };
 
   return (
     <div className="theme-container">
-     <div>
-     <img
+      <img
         onClick={toggleTheme}
         className='theme-toggle-button'
-        src={`${theme === 'light' ? darkModeSvg : lightModeSvg}`}
+        src={theme === 'light' ? darkModeSvg : lightModeSvg}
         alt={`${theme === 'light' ? 'Dark' : 'Light'} Theme`}
       />
-     </div>
-
     </div>
   );
 };
